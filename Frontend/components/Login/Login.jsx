f62fc38e575455bcb84c926db1aa0903f0ab8768
Suraj Pandey <surajpandey7493@gmail.com>
2025-02-24 07:16:57 +0530

    Login screen ui improved

--- Frontend/components/Login/Login.jsx
+++ Frontend/components/Login/Login.jsx
@@ -59,151 +59,165 @@ const Login = ({ navigation }) => {
   };
 
   return (
-    <View style={styles.container}>
-      <View style={styles.topImageContainer}>
-        <Image
-          source={require("./Vector 1.png")}
-          style={styles.topImage}
-        />
+    <>
+    <ImageBackground 
+      source={require('./bgc.jpg')}  
+      style={styles.container}
+    >
+      <Text style={styles.headingText}>Login Here</Text> 
+      <View style={styles.content}>
+        <Text style={styles.contentText}>Welcome Back</Text>
       </View>
-      <View style={styles.helloContainer}>
-        <Text style={styles.helloText}>Bharat Transact</Text>
-      </View>
-      <View>
-        <Text style={styles.signInText}>Sign in to your account</Text>
-      </View>
-      <View style={styles.inputContainer}>
-        <FontAwesome name={"user"} size={24} color={"#949A9A"} style={styles.inputIcon} />
-        <TextInput onChangeText={setEmail} style={styles.textInput} placeholder="Email" />
-      </View>
-      <View style={styles.inputContainer}>
-        <FontAwesome name={"lock"} size={24} color={"#949A9A"} style={styles.inputIcon} />
-        <TextInput onChangeText={setPassword} style={styles.textInput} placeholder="Password" secureTextEntry />
-      </View>
-      <View>
-        <Text 
-          onPress={() => navigation.navigate('ForgotPassword')} 
-          style={styles.forgotPass}
-        >
-          Forgot Your Password?
-        </Text>
-      </View>
-
-      <TouchableOpacity 
-        style={styles.signButtonContainer} 
-        onPress={handleLogin} 
-        disabled={loading} // Disable button during loading
-      >
-        <View style={styles.solidButton}>
-          {loading ? (
-            <ActivityIndicator size="small" color="white" /> // Show loader
-          ) : (
-            <>
-              <Text style={styles.textDesign}>Sign In</Text>
-              <AntDesign name={"arrowright"} size={24} color={"white"} />
-            </>
-          )}
+      <View style={styles.inputs}>
+        <View style={styles.emailContainer}>
+          <FontAwesome name={"envelope"} size={24} color={"#000"} style={styles.inputIcon} />
+          <TextInput 
+            placeholder='Email' 
+            style={styles.textInput} 
+            value={email} 
+            onChangeText={(text) => setEmail(text.toLowerCase())}
+          />
         </View>
-      </TouchableOpacity>
-      <View style={styles.footerTextContainer}>
-        <Text style={styles.footerText}>
-          Don't Have An Account 
-          <Text style={{ textDecorationLine: "underline" }} onPress={handleCreateAccount}> Create One</Text>
-        </Text>
+        <View style={styles.passwordContainer}>
+          <FontAwesome name={"lock"} size={24} color={"#000"} style={styles.inputIcon} />
+          <TextInput 
+            placeholder="Password" 
+            secureTextEntry 
+            style={styles.textInput} 
+            value={password} 
+            onChangeText={(text) => setPassword(text.toLowerCase())} 
+          />
+        </View>
       </View>
-    </View>
-  );
-};
+      <View style={styles.forgotPasswordContainer}>
+        <TouchableOpacity>
+          <Text 
+            onPress={() => navigation.navigate('ForgotPassword')} 
+            style={styles.forgotPassword}
+          >
+            Forgot your password?
+          </Text>
+        </TouchableOpacity>
+      </View>
 
-export default Login;
+      <View style={styles.btns}>
+        <TouchableOpacity 
+          onPress={handleLogin} 
+          disabled={loading} 
+        >
+          <View style={styles.LoginBtn}>
+            {loading ? (
+              <ActivityIndicator size="small" color="white" /> 
+            ) : (
+              <Text style={styles.btnText}>Login</Text>
+            )}
+          </View>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.CreateContainer}>
+                <Text style={styles.CreateContainerText} onPress={handleCreateAccount}> Create New Account </Text>
+            </View>
+    </ImageBackground>
+    </>
+  );
+}
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: "#F5F5F5",
     flex: 1,
-    position: "relative"
-  },
-  topImageContainer: {},
-  topImage: {
-    width: "100%",
-    height: 130,
-  },
-  helloContainer: {},
-  helloText: {
-    textAlign: "center",
-    fontSize: 50,
-    fontWeight: "500",
-    color: "#262626",
-  },
-  signInText: {
-    textAlign: "center",
-    fontSize: 18,
-    color: "#262626",
-    marginBottom: 30,
-  },
-  inputContainer: {
-    backgroundColor: "#FFFFFF",
-    borderRadius: 20,
-    marginHorizontal: 40,
-    elevation: 10,
-    marginVertical: 20,
-    flexDirection: "row", 
-    height: 50, 
+    justifyContent: "flex-start", 
     alignItems: "center",
+    paddingTop: height * 0.2,  
+  },
+  headingText: {
+    color: "#1F41B1",  
+    fontSize: height * 0.08,  
+    fontWeight: "900", 
+  },
+  content: {
+    paddingTop: 25
+  },
+  contentText: {
+    fontSize: height * 0.04,  
+    fontWeight: "900",
+  },
+  inputs: {
+    paddingTop: height * 0.1, 
+    width: "80%", 
+  },
+  emailContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    width: width * 0.8,  
+    height: height * 0.07,  
+    backgroundColor: "#BED8FE",
+    borderRadius: 10,
+    marginBottom: 20,
+    paddingHorizontal: 10,
+    elevation: 5, 
+  },
+  passwordContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    width: width * 0.8, 
+    height: height * 0.07,  
+    backgroundColor: "#BED8FE",
+    borderRadius: 10,
+    paddingHorizontal: 10,
+    elevation: 5, 
   },
   inputIcon: {
-    marginLeft: 15,
+    marginRight: 10, 
   },
   textInput: {
     flex: 1,
-    paddingLeft: 10, 
-    fontSize: 16, 
+    fontSize: height * 0.02,  
+    paddingVertical: 10,
   },
-  forgotPass: {
-    fontSize: 15,
-    textAlign: "right",
-    marginRight: 20,
-    marginTop: 25,
+  forgotPasswordContainer: {
+    width: width * 0.8,  
+    marginTop: 10,
+    alignItems: 'flex-end',  
   },
-  signButtonContainer: {
-    flexDirection: "row",
-    marginTop: 40, 
-    justifyContent: "flex-end",
-    width: "90%",
-    color: "black"
+  forgotPassword: {
+    color: '#1F41BB',  
+    fontSize: height * 0.02,  
+    textAlign: 'right',
+    marginTop: 10,
   },
-  solidButton: {
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "center",
-    paddingVertical: 12,
-    paddingHorizontal: 20,
-    backgroundColor: "#FF5722", 
-    borderRadius: 25,
+  btns: {
+    paddingTop: 20,
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: "center"
   },
-  textDesign: {
-    fontSize: 15,
-    fontWeight: "bold",
-    color: "white",
-    marginRight: 10, 
+  LoginBtn: {
+    fontSize: height * 0.025,  
+    color: 'white',
+    backgroundColor: '#1F41BB',
+    borderRadius: 6,
+    paddingVertical: 12,  
+    paddingHorizontal: 50,  
+    fontWeight: "700",
+    width: width * 0.8,  
+    textAlign: "center",
+    justifyContent: 'center',
+    alignItems: 'center',
   },
-  footerTextContainer: {
+  btnText: {
+    fontSize: height * 0.025,  
+    color: 'white',
+    fontWeight: "700",
+  },
+  CreateContainer: {
     textAlign: "center",
     marginTop: 50,
   },
-  footerText: {
-    color: "Black",
+  CreateContainerText: {
+    color: "#1F41B1",
     textAlign: "center",
-    fontSize: 15,
+    fontSize: 18,
+    fontWeight:'900'
   },
-  leftImageContainer: {
-    position: "absolute",
-    zIndex:0,
-    bottom: 0,
-    left: 0,
-  },
-  leftImage: {
-    height: 500,
-    width: 230,
-  }
 });
+
